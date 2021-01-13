1fbe0945fda25e6cc2be0af767ccd48e11eddc24
AdityaRawEngg <aditya.shukla@raweng.com>
2021-01-13 15:33:07 +0530

    Added event listiners
    Added a event listiner for login, signup and logout button

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,7 +4,15 @@ import Cookies from "js-cookies";
 function Navigation(props) {
   const logout = () => {
     Cookies.removeItem("jwt");
+    props.history.push("/login");
   };
+  const signUp = () => {
+    props.history.push("/signup");
+  };
+  const login = () => {
+    props.history.push("/login");
+  };
+
   return (
     <nav>
       <Link to={"/"}>
@@ -14,11 +22,17 @@ function Navigation(props) {
         />
       </Link>
       {props.location.pathname === "/signup" ? (
-        <button className={styles["logout-btn"]}>Login</button>
+        <button onClick={login} className={styles["logout-btn"]}>
+          Login
+        </button>
       ) : props.location.pathname === "/login" ? (
-        <button className={styles["logout-btn"]}>Signin</button>
+        <button onClick={signUp} className={styles["logout-btn"]}>
+          Signup
+        </button>
       ) : (
-        <button className={styles["logout-btn"]}>Logout</button>
+        <button onClick={logout} className={styles["logout-btn"]}>
+          Logout
+        </button>
       )}
     </nav>
   );
